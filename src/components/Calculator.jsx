6b30454702a0b3724eeb185eacf507f5c81bc16b
Miaad Estakhri <94445130+MiaadEstakhri@users.noreply.github.com>
2023-01-12 12:56:29 +0330

    create Calculator component

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,11 @@
+import Wrapper from "./Wrapper";
+
+const Calculator = () => {
+  return (
+    <main className="d-flex justify-content-center align-items-center">
+      <Wrapper></Wrapper>
+    </main>
+  );
+};
+
+export default Calculator;
