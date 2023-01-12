4f440833a87394c7005953d1777842ffd8857244
Miaad Estakhri <94445130+MiaadEstakhri@users.noreply.github.com>
2023-01-12 20:41:45 +0330

    create useState and using number and useNumber

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -2,33 +2,40 @@ import Wrapper from "./Wrapper";
 import Screen from "./Screen";
 import ButtonBox from "./ButtonBox";
 import Button from "./Button";
+import { useState } from "react";
 
 const Calculator = () => {
+  const [number, setNumber] = useState("");
+  console.log(number);
   return (
     <main className="d-flex justify-content-center align-items-center">
       <Wrapper>
-        <Screen />
+        <Screen value={number} />
         <ButtonBox>
-          <Button item="Ac" />
-          <Button item="+/-" />
-          <Button item="%" />
-          <Button item="/" />
-          <Button item="7" />
-          <Button item="8" />
-          <Button item="9" />
-          <Button item="x" />
-          <Button item="4" />
-          <Button item="5" />
-          <Button item="6" />
-          <Button item="-" />
-          <Button item="1" />
-          <Button item="2" />
-          <Button item="3" />
-          <Button item="+" />
-          <Button item="0" />
-          <Button item="." />
-          <Button item="=" />
-          <Button className="fs-4" item="C" />
+          <Button item="Ac" onclick={() => setNumber("")} />
+          <Button item="+/-" onclick={() => setNumber(number + "1")} />
+          <Button item="%" onclick={() => setNumber(number / 100)} />
+          <Button item="/" onclick={() => setNumber(number + "/")} />
+          <Button item="7" onclick={() => setNumber(number + "7")} />
+          <Button item="8" onclick={() => setNumber(number + "8")} />
+          <Button item="9" onclick={() => setNumber(number + "9")} />
+          <Button item="x" onclick={() => setNumber(number + " * ")} />
+          <Button item="4" onclick={() => setNumber(number + "4")} />
+          <Button item="5" onclick={() => setNumber(number + "5")} />
+          <Button item="6" onclick={() => setNumber(number + "6")} />
+          <Button item="-" onclick={() => setNumber(number + " - ")} />
+          <Button item="1" onclick={() => setNumber(number + "1")} />
+          <Button item="2" onclick={() => setNumber(number + "2")} />
+          <Button item="3" onclick={() => setNumber(number + "3")} />
+          <Button item="+" onclick={() => setNumber(number + " + ")} />
+          <Button item="0" onclick={() => setNumber(number + "0")} />
+          <Button item="." onclick={() => setNumber(number + ".")} />
+          <Button item="=" onclick={() => setNumber(number + "=")} />
+          <Button
+            className="fs-4"
+            item="C"
+            onclick={() => setNumber(number.slice(0, -2))}
+          />
         </ButtonBox>
       </Wrapper>
     </main>
