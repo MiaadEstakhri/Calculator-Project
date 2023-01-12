6df80a87a53bb5a65e0181c6925f57f292039844
Miaad Estakhri <94445130+MiaadEstakhri@users.noreply.github.com>
2023-01-12 12:57:00 +0330

    Wrapper component

---
+++ src/components/Wrapper.jsx
@@ -0,0 +1,9 @@
+const Wrapper = ({ children }) => {
+  return (
+    <section className="d-flex flex-column justify-content-center">
+      {children}
+    </section>
+  );
+};
+
+export default Wrapper;
