2c4da2456f1b3b9b98c4877cb0443aca43ae6453
Miaad Estakhri <94445130+MiaadEstakhri@users.noreply.github.com>
2023-01-13 15:02:15 +0330

    create pm function

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,9 @@
-const Button = ({ item, onclick, className }) => {
+const Button = ({ item, onclick, className,value }) => {
   return (
     <button
       className={`${className} fs-2 border-0 fw-bold p-2 `}
       onClick={onclick}
+      value={value}
     >
       {item}
     </button>
--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -56,19 +56,39 @@ const Calculator = () => {
     }
   };
 
+  const pm = (e) => {
+    const value = e.target.value;
+
+    if (value === "pm") {
+      if (number === "") return;
+      let calc;
+      if (Number(number.slice(-1))) {
+        calc = eval(number);
+        if (Math.sign(calc) < 0) {
+          calc = Math.abs(calc);
+          setNumber(calc.toString());
+          return;
+        } else {
+          setNumber("-" + calc.toString());
+          return;
+        }
+      }
+    }
+  };
+
   return (
     <main className="d-flex justify-content-center align-items-center">
       <Wrapper>
-        <Screen value={number} />
+        <Screen value={number ? number : 0} />
         <ButtonBox>
           <Button item="Ac" onclick={() => setNumber("")} />
-          <Button item="+/-" onclick={() => setNumber(number * "-1")} />
+          <Button item="+/-" value={"pm"} onclick={pm} />
           <Button item="%" onclick={() => setNumber(number / 100)} />
           <Button item="/" onclick={() => setNumber(number + " / ")} />
           <Button item="7" onclick={() => setNumber(number + "7")} />
           <Button item="8" onclick={() => setNumber(number + "8")} />
           <Button item="9" onclick={() => setNumber(number + "9")} />
-          <Button item="x" onclick={() => setNumber(number + " * ")} />
+          <Button item="x" onclick={p} />
           <Button item="4" onclick={() => setNumber(number + "4")} />
           <Button item="5" onclick={() => setNumber(number + "5")} />
           <Button item="6" onclick={() => setNumber(number + "6")} />
