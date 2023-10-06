01f1c9ddd3a73413fe1c44051a32ef958c48e57c
MiaadEstakhri <miaad8104@gmail.com>
2023-10-06 23:39:46 +0330

    I added onChange to the input to fix the console error

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -1,7 +1,11 @@
 const Screen = ({ value }) => {
   return (
     <section className="w-100 d-flex justify-content-center align-items-center">
-      <input className="w-100  rounded-3 px-2 py-3 h3 m-2" value={value} />
+      <input
+        className="w-100  rounded-3 px-2 py-3 h3 m-2"
+        value={value}
+        onChange={() => {}}
+      />
     </section>
   );
 };
